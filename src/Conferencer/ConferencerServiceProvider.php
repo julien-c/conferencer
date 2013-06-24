7ae004deb62c76650a221b3c39e4b0506d24306c
Maxime Fabre <ehtnam6@gmail.com>
2013-06-24 11:44:23 +0200

    Move some code in the ConferencerServiceProvider::boot method

--- src/Conferencer/ConferencerServiceProvider.php
+++ src/Conferencer/ConferencerServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 namespace Conferencer;
 
+use HTML;
 use Illuminate\Support\ServiceProvider;
 
 class ConferencerServiceProvider extends ServiceProvider
@@ -25,11 +26,34 @@ class ConferencerServiceProvider extends ServiceProvider
 
 		// Register views
 		$this->app['view']->addNamespace('conferencer', __DIR__.'/../../views');
+	}
+
+	/**
+	 * Boot Conferencer
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		// Register macros
+		$this->registerMacros();
 
 		// Register Routes and View composers
 		include __DIR__.'/../assets.php';
 		include __DIR__.'/../composers.php';
 		include __DIR__.'/../routes.php';
+	}
+
+	/**
+	 * Register custom HTML macros
+	 *
+	 * @return void
+	 */
+	protected function registerMacros()
+	{
+		HTML::macro('linkBlank', function($url, $value = null) {
+			return HTML::link($url, $value, array('target' => '_blank'));
+		});
 	}
 
 	/**

a10f43d05619906d98cdfbb30a5f50c81ee3b88d
Maxime Fabre <ehtnam6@gmail.com>
2013-06-25 11:27:47 +0200

    Add SVG polyfill to polyfill.js

--- public/js/polyfill.js
+++ public/js/polyfill.js
@@ -1,4 +1,6 @@
 (function() {
+
+	// Load Respond and Selectivizr polyfills if necessary
   Modernizr.load([
     {
       test: Modernizr.mediaqueries,
@@ -8,5 +10,22 @@
       nope: '//cdnjs.cloudflare.com/ajax/libs/selectivizr/1.0.2/selectivizr-min.js'
     }
   ]);
+
+  // Replace SVG images with JPG if unsupported
+  if (!Modernizr.svg) {
+
+  	// Loop over the images in the current page
+    images = document.getElementsByTagName('img');
+    for (_i = 0, _len = images.length; _i < _len; _i++) {
+      image = images[_i];
+
+      // If it's an SVG image, replace src attribute
+      src = image.getAttribute('src');
+      if (src.match('.svg')) {
+      	src = src.replace('.svg', '.jpg');
+        image.setAttribute('src', src);
+      }
+    }
+  }
 
 }).call(this);
